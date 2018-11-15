51976b784da8e920b94a6940cfeee8e46a9ee120
Roderik van der Veer <roderik@settlemint.com>
2018-11-15 10:40:24 +0400

    fix: jest issues

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,4 @@
 module.exports = {
-  globals: {
-    'ts-jest': {
-      tsConfig: 'tsconfig.json'
-    }
-  },
-  roots: ['<rootDir>/src'],
-  modulePaths: ['src', 'node_modules'],
-  moduleFileExtensions: ['js', 'ts'],
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest'
-  },
+  preset: 'ts-jest',
   testEnvironment: 'node',
-  setupTestFrameworkScriptFile: './jest.setup.js',
-  testMatch: ['**/test/**/*.test.(ts|js)'],
-  preset: 'ts-jest'
 };
